a28a11cf943fd30c3beca03c840250e8b403c3d9
Bernie White <bewhite@microsoft.com>
2019-09-02 21:42:40 +1000

    Update scope and manifest

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -2,15 +2,19 @@
 $workspacePath = $Env:GITHUB_WORKSPACE;
 $sourcePath = Join-Path -Path $workspacePath -ChildPath $Env:INPUT_SOURCE;
 
-Write-Host "> Preparing PSRule";
+Write-Host '> Preparing PSRule';
 Import-Module -Name /ps-rule/modules/PSRule;
 
 Write-Host "> Using source: $sourcePath";
 Write-Host "> Using workspace: $workspacePath";
 
+Write-Host '> Executing rules';
+
 try {
     Get-ChildItem -Path $workspacePath -File | Invoke-PSRule -Path $sourcePath -ErrorAction Stop;
 }
-finally {
+catch {
     $Host.SetShouldExit(1);
 }
+
+Write-Host '> All done.';
