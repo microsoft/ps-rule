c5d2890d887f9b94aee96c68a4482e11f691edfc
Bernie White <bewhite@microsoft.com>
2019-09-11 22:38:07 +1000

    Add CI workflow (#3)

---
+++ .ps-rule/Creds.Rule.ps1
@@ -0,0 +1,4 @@
+
+Rule 'Repo.Creds.PS1' -Type 'System.IO.FileInfo' -If { $TargetObject.Extension -eq '.ps1' -and $TargetObject.FullName -notlike '*.ps-rule*'} {
+    (Get-Content -Path $TargetObject.FullName -Raw) -notlike "*ConvertTo-SecureString*-AsPlainText*"
+}
