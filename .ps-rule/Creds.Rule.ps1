1ec65d7ee9fd0c6e93cd27f0dd0176fd47cc9c8e
Bernie White <bewhite@microsoft.com>
2019-10-07 20:33:28 +1000

    Add rules

--- .ps-rule/Creds.Rule.ps1
+++
@@ -1,4 +0,0 @@
-
-Rule 'Repo.Creds.PS1' -Type 'System.IO.FileInfo' -If { $TargetObject.Extension -eq '.ps1' -and $TargetObject.FullName -notlike '*.ps-rule*'} {
-    (Get-Content -Path $TargetObject.FullName -Raw) -notlike "*ConvertTo-SecureString*-AsPlainText*"
-}
---
+++ .ps-rule/Repos.GitHub.Rule.ps1
@@ -0,0 +1,16 @@
+
+# Synopsis: Check for recommended community files
+Rule 'Repos.GH.Contribute' -Type 'System.IO.DirectoryInfo' {
+    $requiredFiles = @(
+        'CHANGELOG.md'
+        'LICENSE'
+        '.github/CODEOWNERS'
+        '.github/PULL_REQUEST_TEMPLATE.md'
+    )
+    Test-Path -Path $TargetObject.FullName;
+
+    for ($i =0; $i -lt $requiredFiles.Length; $i++) {
+        $filePath = Join-Path -Path $TargetObject.FullName -ChildPath $requiredFiles[$i];
+        $Assert.Create((Test-Path -Path $filePath -PathType Leaf), "$($requiredFiles[$i]) does not exist");
+    }
+}
