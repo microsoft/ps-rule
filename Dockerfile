fe7ae68540c17f35e42cb7b84c700c57e263b637
Bernie White <bernardawhite@hotmail.com>
2020-04-17 17:14:18 +1000

    Use Assert instead of Invoke (#5)

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
 #
 # PSRule
 # 
-FROM mcr.microsoft.com/powershell:6.2.3-alpine-3.8
+FROM mcr.microsoft.com/powershell:7.0.0-alpine-3.8
 SHELL ["pwsh", "-Command"]
-RUN New-Item -Path /ps-rule/modules/ -ItemType Directory -Force; \
-    Save-Module -Name PSRule -Force -Path /ps-rule/modules/;
+RUN $ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue; \
+    $Null = New-Item -Path /ps_modules/ -ItemType Directory -Force; \
+    Save-Module -Name PSRule -RequiredVersion 0.16.0 -Force -Path /ps_modules/; \
+    Save-Module -Name PSRule.Rules.Azure -RequiredVersion 0.10.1 -Force -Path /ps_modules/; \
+    Save-Module -Name PSRule.Rules.Kubernetes -RequiredVersion 0.2.0 -Force -Path /ps_modules/;
 
 COPY LICENSE README.md /
-COPY entrypoint.ps1 /entrypoint.ps1
+COPY powershell.ps1 /powershell.ps1
 
-CMD ["pwsh", "-File", "/entrypoint.ps1"]
+CMD ["pwsh", "-File", "/powershell.ps1"]
---
+++ powershell.ps1
@@ -0,0 +1,190 @@
+#
+# PSRule
+#
+
+# See details at: https://github.com/BernieWhite/PSRule-actions
+
+[CmdletBinding()]
+param (
+    # The root path for analysis
+    [Parameter(Mandatory = $False)]
+    [String]$Path = $Env:INPUT_PATH,
+
+    # The type of input
+    [Parameter(Mandatory = $False)]
+    [ValidateSet('repository', 'inputPath')]
+    [String]$InputType = $Env:INPUT_INPUTTYPE,
+
+     # The path to input files
+     [Parameter(Mandatory = $False)]
+     [String]$InputPath = $Env:INPUT_INPUTPATH,
+
+    # The path to find rules
+    [Parameter(Mandatory = $False)]
+    [String]$Source = $Env:INPUT_SOURCE,
+
+    # Rule modules to use
+    [Parameter(Mandatory = $False)]
+    [String]$Modules = $Env:INPUT_MODULES,
+
+    # The output format
+    [Parameter(Mandatory = $False)]
+    [ValidateSet('None', 'Yaml', 'Json', 'NUnit3', 'Csv')]
+    [String]$OutputFormat = $Env:INPUT_OUTPUTFORMAT,
+
+    # The path to store formatted output
+    [Parameter(Mandatory = $False)]
+    [String]$OutputPath = $Env:INPUT_OUTPUTPATH
+)
+
+$workspacePath = $Env:GITHUB_WORKSPACE;
+$ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue;
+if ($Env:SYSTEM_DEBUG -eq 'true') {
+    $VerbosePreference = [System.Management.Automation.ActionPreference]::Continue;
+}
+
+# Set workspace
+if ([String]::IsNullOrEmpty($workspacePath)) {
+    $workspacePath = $PWD;
+}
+
+# Set Path
+if ([String]::IsNullOrEmpty($Path)) {
+    $Path = $workspacePath;
+}
+else {
+    $Path = Join-Path -Path $workspacePath -ChildPath $Path;
+}
+
+# Set InputPath
+if ([String]::IsNullOrEmpty($InputPath)) {
+    $InputPath = $Path;
+}
+else {
+    $InputPath = Join-Path -Path $Path -ChildPath $InputPath;
+}
+
+# Set Source
+if ([String]::IsNullOrEmpty($Source)) {
+    $Source = Join-Path -Path $Path -ChildPath '.ps-rule/';
+}
+else {
+    $Source = Join-Path -Path $Path -ChildPath $Source;
+}
+
+function WriteDebug {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, Mandatory = $True)]
+        [String]$Message
+    )
+    process {
+        if ($Env:SYSTEM_DEBUG -eq 'true') {
+            Write-Host "[debug] $Message";
+        }
+    }
+}
+
+# Setup paths for importing modules
+$modulesPath = '/ps_modules/';
+if ((Get-Variable -Name IsMacOS -ErrorAction Ignore) -or (Get-Variable -Name IsLinux -ErrorAction Ignore)) {
+    $moduleSearchPaths = $Env:PSModulePath.Split(':', [System.StringSplitOptions]::RemoveEmptyEntries);
+    if ($modulesPath -notin $moduleSearchPaths) {
+        $Env:PSModulePath += [String]::Concat($Env:PSModulePath, ':', $modulesPath);
+    }
+}
+else {
+    $moduleSearchPaths = $Env:PSModulePath.Split(';', [System.StringSplitOptions]::RemoveEmptyEntries);
+    if ($modulesPath -notin $moduleSearchPaths) {
+        $Env:PSModulePath += [String]::Concat($Env:PSModulePath, ';', $modulesPath);
+    }
+}
+
+$moduleNames = @()
+if (![String]::IsNullOrEmpty($Modules)) {
+    $moduleNames = $Modules.Split(',', [System.StringSplitOptions]::RemoveEmptyEntries);
+}
+$moduleParams = @{
+    Scope = 'CurrentUser'
+    Force = $True
+}
+
+Write-Host "`#`#[group] Preparing PSRule";
+
+# Install each module if not already installed
+foreach ($m in $moduleNames) {
+    $m = $m.Trim();
+    Write-Host "> Checking module: $m";
+    if ($Null -eq (Get-InstalledModule -Name $m -ErrorAction Ignore)) {
+        Write-Host '  - Installing module';
+        $Null = Install-Module -Name $m @moduleParams -AllowClobber;
+    }
+    else {
+        Write-Host '  - Already installed';
+    }
+    # Check
+    if ($Null -eq (Get-InstalledModule -Name $m)) {
+        Write-Host "  - Failed to install";
+    }
+    else {
+        Write-Host "  - Using version: $((Get-InstalledModule -Name $m).Version)";
+    }
+}
+
+Write-Host '';
+Write-Host "[info] Using Path: $Path";
+Write-Host "[info] Using Source: $Source";
+Write-Host "[info] Using InputType: $InputType";
+Write-Host "[info] Using InputPath: $InputPath";
+Write-Host "[info] Using OutputFormat: $OutputFormat";
+Write-Host "[info] Using OutputPath: $OutputPath";
+
+Write-Host "`#`#[endgroup]";
+
+try {
+    Push-Location -Path $Path;
+    $invokeParams = @{
+        Path = $Source
+        Option = (New-PSRuleOption -TargetName 'FullName')
+        Style = 'GitHubActions'
+        ErrorAction = 'Stop'
+    }
+    WriteDebug "Preparing command-line:";
+    WriteDebug ([String]::Concat('-Path ''', $Source, ''''));
+    if (![String]::IsNullOrEmpty($Modules)) {
+        $moduleNames = $Modules.Split(',', [System.StringSplitOptions]::RemoveEmptyEntries);
+        $invokeParams['Module'] = $moduleNames;
+        WriteDebug ([String]::Concat('-Module ', [String]::Join(', ', $moduleNames)));
+    }
+    if (![String]::IsNullOrEmpty($OutputFormat) -and ![String]::IsNullOrEmpty($OutputPath) -and $OutputFormat -ne 'None') {
+        $invokeParams['OutputFormat'] = $OutputFormat;
+        $invokeParams['OutputPath'] = $OutputPath;
+        WriteDebug ([String]::Concat('-OutputFormat ', $OutputFormat, ' -OutputPath ''', $OutputPath, ''''));
+    }
+
+    # Repository
+    if ($InputType -eq 'repository') {
+        $items = New-Object -TypeName System.Collections.ArrayList;
+        WriteDebug 'Running ''Assert-PSRule'' with repository as input.';
+        $Null = $items.Add((Get-Item -Path $InputPath));
+        $Null = $items.AddRange((Get-ChildItem -Path $InputPath -File -Recurse));
+        Write-Host '';
+        Write-Host '---';
+        $items.ToArray() | Assert-PSRule @invokeParams;
+    }
+    # Repository
+    elseif ($InputType -eq 'inputPath') {
+        WriteDebug 'Running ''Assert-PSRule'' with input from path.';
+        Write-Host '';
+        Write-Host '---';
+        Assert-PSRule @invokeParams -InputPath $InputPath;
+    }
+}
+catch {
+    Write-Host "::error::One or more assertions failed.";
+    $Host.SetShouldExit(1);
+}
+finally {
+    Pop-Location;
+}
+Write-Host '---';
